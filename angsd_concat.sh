54be6a944982c635b15da1cb35db74375ba4b4cb
jaelynbos <jbos@ucsc.edu>
2026-03-18 18:49:51 -0700

    initial commit

---
+++ angsd_concat.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH --job-name=angsd_concat
+#SBATCH -o angsd_out/angsd_concat-%j.out
+#SBATCH --partition=lab-mpinsky
+#SBATCH --qos=pi-mpinsky
+#SBATCH --account=pi-mpinsky
+#SBATCH --cpus-per-task=2
+#SBATCH --mail-user=jbos@ucsc.edu
+#SBATCH --mail-type=ALL
+#SBATCH --mem=64G
+#SBATCH --time=24:00:00
+
+module load samtools
+
+OUTDIR=/scratch/jbos/Moz_intermediates/beagle_contigs_spp1
+
+first=1
+for f in ${OUTDIR}/Acorpora_moz.*.beagle.gz; do
+    if [ $first -eq 1 ]; then
+        zcat $f
+        first=0
+    else
+        zcat $f | tail -n +2  # skip header line for all subsequent files
+    fi
+done | bgzip > ${OUTDIR}/Acorpora_moz_all.beagle.gz
+
+
+first=1
+for f in ${OUTDIR}/Acorpora_moz.*.mafs.gz; do
+    if [ $first -eq 1 ]; then
+        zcat $f
+        first=0
+    else
+        zcat $f | tail -n +2  # skip header line for all subsequent files
+    fi
+done | bgzip > ${OUTDIR}/Acorpora_moz_all.mafs.gz
