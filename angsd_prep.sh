1f7aae1ec574be1f3d127e750635dffdad7bfce8
jaelynbos <jbos@ucsc.edu>
2026-07-11 18:05:37 -0700

    misc

--- angsd_prep.sh
+++ angsd_prep.sh
@@ -14,13 +14,13 @@
 module load samtools
 
 #Make list of bamfiles
-ls /scratch/jbos/Moz_intermediates/amuricata_bamfiles/*.bam > /home/jbos/Moz_reads/bamlist.txt
+ls /scratch/jbos/Moz_aligned_mil/amillepora_bamfiles/*.bam > /home/jbos/Moz_reads/bamlist_amillepora.txt
 
 #Samtools index reference
-REF=/home/jbos/ncbi/amuricata_ncbi.fna
+REF=/home/jbos/ncbi/GCF_013753865.1_Amil_v2.1_genomic.fna
 samtools faidx $REF
 
 #Make list of contigs
-cut -f1 /home/jbos/ncbi/amuricata_ncbi.fna.fai > /home/jbos/Moz_reads/contig_list.txt
+cut -f1 /home/jbos/ncbi/GCF_013753865.1_Amil_v2.1_genomic.fna.fai > /home/jbos/Moz_reads/contig_list_Amillepora.txt
 
 
