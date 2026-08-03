e1afbe0b4fa2c6c330d86ad0d6e4cc4440d43a0c
jaelynbos <jbos@ucsc.edu>
2026-08-03 10:19:21 -0700

    fixed HES1 window size

--- mafft_nucleotide.sh
+++ mafft_nucleotide.sh
@@ -9,8 +9,7 @@
 #SBATCH --time=72:00:00
 
 module load samtools
-module load miniconda3
-conda activate mafft 
+module load samtools
 
 OUTDIR=/scratch/jbos/hes1_sequences
 
@@ -23,7 +22,7 @@ do
 	samtools faidx $sample.fa.gz NC_058072.1:20450110-20468842 > $OUTDIR/${sample}_mRNA3.fasta # old gene 12599 is in here
 	samtools faidx $sample.fa.gz NC_058072.1:20495840-20513836 > $OUTDIR/${sample}_mRNA5.fasta
 	samtools faidx $sample.fa.gz NC_058072.1:20483176-20486793 > $OUTDIR/${sample}_noncoding2.fasta
-	samtools faidx $sample.fa.gz NC_058072.1:20350000-205700010 > $OUTDIR/${sample}_hes1_rose.fasta 
+	samtools faidx $sample.fa.gz NC_058072.1:20350000-20570000 > $OUTDIR/${sample}_hes1_rose.fasta
 done
 
 cd /scratch/jbos/consensus_fastas
@@ -33,7 +32,7 @@ do
 	samtools faidx $sample.fa.gz NC_058072.1:20450110-20468842 > $OUTDIR/${sample}_mRNA3.fasta
 	samtools faidx $sample.fa.gz NC_058072.1:20495840-20513836 > $OUTDIR/${sample}_mRNA5.fasta
 	samtools faidx $sample.fa.gz NC_058072.1:20483176-20486793 > $OUTDIR/${sample}_noncoding2.fasta
-	samtools faidx $sample.fa.gz NC_058072.1:20350000-205700010 > $OUTDIR/${sample}_hes1_rose.fasta
+	samtools faidx $sample.fa.gz NC_058072.1:20350000-20570000 > $OUTDIR/${sample}_hes1_rose.fasta
 done
 
 cd $OUTDIR
@@ -42,6 +41,9 @@ cat *_mRNA3.fasta > mRNA3.fasta
 cat *_mRNA5.fasta > mRNA5.fasta
 cat *_hes1_rose.fasta > hes1_rose.fasta
 cat *_noncoding2.fasta > noncoding2.fasta
+
+module load miniconda3
+conda activate mafft 
 
 mafft --auto mRNA3.fasta > mRNA3.aln
 mafft --auto mRNA5.fasta > mRNA5.aln
